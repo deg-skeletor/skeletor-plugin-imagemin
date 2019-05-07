3d49e651c53d48622bc7c9487152f98a7e0d7ec3
Joseph Lawhorn <jlawhorn@degdigital.com>
2019-05-07 15:26:52 -0500

    Try correct plugin.

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
-const fs = require('fs');
-const imagemin = require('image-min');
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminWebp = require('imagemin-webp');
+const imageminSvgo = require('imagemin-svgo');
+const imageminGiflossy = require('imagemin-giflossy');
+
+const fs = require('fs-extra');
 const path = require('path');
 
 const run = (config, options) => {
-    const sourceFile = options.source ? options.source.filename : null;
-    return new Promise((resolve, reject) => {
-        try {
-            const src = fs.createReadStream(sourceFile);
-            const ext = path.extname(src.path);
-            src.pipe(imagemin({ext: ext})).pipe(fs.createWriteStream(`img-minified${ext}`));
-            resolve({
-				status: 'complete',
-				message: 'Sample plugin complete'
-			});
-		} catch(e) {
-			reject({
-				status: 'error',
-				message: e.message
-			});
-		}
-    });
-}
-
-module.exports = skeletorImageOptimizer = () => (
-    {
-        run
+    if (!Array.isArray(config.files)) {
+        return Promise.resolve({
+            status: 'error',
+            message: 'Configuration does not contain a valid "files" property.'
+        });
     }
-);
+    const promises = config.files.map(fileConfig => processFile(fileConfig, config.plugins, logger));
+
+    return Promise.all(promises)
+        .then(() => {
+            const message = `${config.files.length} image(s) processed.`;
+            logger.info(message);
+            return {
+                status: 'complete',
+                message: message
+            };
+        })
+        .catch(e => {
+            logger.error(e);
+            return {
+                status: 'error',
+                message: e.message
+            };
+        });
+};
+
+const processFile = (fileConfig, plugins, logger) => {
+    const srcFilepath = path.resolve(process.cwd(), fileConfig.src);
+    const destinationArray = getDestinations(fileConfig);
+    const params = {plugins, logger, srcFilepath, destinationArray};
+    const promises = initOutput(params);
+    return Promise.all(promises);
+};
+
+const getDestinations = fileConfig => {
+    if (Array.isArray(fileConfig.dest)) {
+        return [...fileConfig.dest];
+    }
+    return [fileConfig.dest];
+};
+
+const initOutput = params => {
+    const {plugins, logger, srcFilepath, destinationArray} = params;
+    return destinationArray.map(dest => {
+        const destFilepath = path.resolve(process.cwd(), dest);
+        return fs
+            .readFile(
+                srcFilepath
+            ).then(contents =>
+                imagemin(
+                    [srcFilepath + '/*.{jpg,png,svg,webp,gif}'],
+                    destFilepath,
+                    plugins
+                );
+            ).then(
+                result => {
+                    displayWarnings(result, logger);
+                    return fs.ensureDir(path.dirname(destFilepath)).then(() => fs.writeFile(destFilepath, result.css));
+            });
+    });
+};
+
+const displayWarnings = (result, logger) => {
+    const warnings = result.warnings();
+    warnings.forEach(warning => logger.warn(`${warning.type}: ${warning.text}`));
+};
+
+module.exports = skeletorImageOptimizer = () => ({
+    run
+});
